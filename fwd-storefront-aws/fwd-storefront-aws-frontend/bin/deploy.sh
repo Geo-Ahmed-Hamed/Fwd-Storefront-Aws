c9ab8a4c320a8a71de931cbc376870357e351cc1
Ahmed Mohamed Hamed <amhamed@EFG-Hermes.com>
2023-02-25 12:03:17 +0200

    update

--- fwd-storefront-aws/fwd-storefront-aws-frontend/bin/deploy.sh
+++ fwd-storefront-aws/fwd-storefront-aws-frontend/bin/deploy.sh
@@ -1,4 +1,3 @@
 aws s3 cp --recursive  ./www s3://udagram-hamed/
-aws s3 cp --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://udagram-hamed/
 # aws s3 cp --recursive --acl public-read ./www s3://udagram-hamed/
 # aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://udagram-hamed/
